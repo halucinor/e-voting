e83794649a68965ca14fe857539c942b38b5e825
백승주 <sjbaek@gabia.com>
2023-01-06 16:55:31 +0900

    spring boot init commit

---
+++ src/main/java/com/gabia/evoting/EVotingApplication.java
@@ -0,0 +1,15 @@
+package com.gabia.evoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVotingApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EVotingApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/gabia/evoting/EVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabia.evoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
