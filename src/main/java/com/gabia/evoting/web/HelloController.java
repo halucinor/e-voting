15bd59a5f260e077b3b1ec875eb02a706591cef6
백승주 <sjbaek@gabia.com>
2023-01-09 17:27:06 +0900

    swagger setting

--- src/main/java/com/gabia/evoting/web/HelloController.java
+++ src/main/java/com/gabia/evoting/web/HelloController.java
@@ -2,7 +2,9 @@ package com.gabia.evoting.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
     @GetMapping("/api/hello1")
     public String hello1() {
